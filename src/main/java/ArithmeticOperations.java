ee26bacdff66ef45d2bdd94685755c3740a39e10
felipe <felipemontana@gmail.com>
2018-01-13 14:41:06 -0500

    added multiple() function

---
+++ src/main/java/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+public final class ArithmeticOperations {
+
+    private ArithmeticOperations() throws InstantiationException {
+        throw new InstantiationException("class should not be instantiated");
+    }
+
+    public static boolean multiple(int a, int b){
+        if(b == 0){
+            throw new IllegalArgumentException();
+        }
+        return a % b == 0;
+    }
+
+}
